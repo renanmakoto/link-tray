0ecbb6a82837dc7cbad08f429f9f5ebc116d6fb0
Renan Makoto <65373130+renanmakoto@users.noreply.github.com>
2024-06-01 22:22:56 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -5,18 +5,17 @@ function toggleMode() {
   const paragraph = document.querySelector("#paragraph")
   paragraph.innerHTML = "Fundo claro e usando preto."
 
-
-  // pegar a tag img
   const img = document.querySelector("#profile img")
 
-  // substituir a imagem
   if (html.classList.contains("light")) {
-    // se tiver light mode, adicionar a imagem light
+
     img.setAttribute("src", "./assets/avatar-light.jpg")
     img.style.borderRadius = "100px"
+
   } else {
-    // set tiver sem light mode, manter a imagem normal
+
     img.setAttribute("src", "./assets/avatar.jpg")
     paragraph.innerHTML = "Cineasta frustrado, só uso preto."
+    
   }
 }
